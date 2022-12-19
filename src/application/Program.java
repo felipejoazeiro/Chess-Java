4f9458e293ba734f70e3a913389cb5d5cde0e5f1
felipejoazeiro <felipejoazeiro@gmail.com>
2022-12-19 10:56:21 -0300

    Pieces moviment

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,11 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
+
 import application.UI;
 
 public class Program {
@@ -10,9 +15,22 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		while(true) {			
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
 		
-		UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		
+		}
 	}
 
 }
